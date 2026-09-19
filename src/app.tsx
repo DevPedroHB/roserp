9b8512c9722ec70c23cb4afb82057d464bf8cb88
Pedro Henrique Bergamo <pedrohb.dev@outlook.com>
2026-09-19 02:44:30 +0000

    🎉 Commit inicial

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="place-content-center gap-4 grid bg-zinc-900 p-4 w-full min-h-svh text-zinc-100">
+      <h1>RoseRP</h1>
+    </main>
+  );
+}
