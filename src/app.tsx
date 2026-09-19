99b72559e51d189c7841f02b27ae0ac7ac8f53bc
Pedro Henrique Bergamo <pedrohb.dev@outlook.com>
2026-09-19 01:09:18 -0300

    feat: connect React Toast to FiveM NUI events

--- src/app.tsx
+++ src/app.tsx
@@ -1,44 +1,37 @@
+import { useCallback } from "react";
 import { Button } from "./components/ui/button";
 import { toast } from "./components/ui/toast";
+import { useNuiEvent } from "./hooks/use-nui-event";
+
+type ToastMessage = {
+    title: string;
+    description?: string;
+    type: "success" | "info" | "warning" | "error" | "loading";
+};
 
 export default function App() {
-	function handleOpenToast() {
-		toast.add({
-			title: "Sucesso",
-			description: "Um exemplo de sucesso.",
-			type: "success",
-		});
-		toast.add({
-			title: "Aviso",
-			description: "Um exemplo de aviso.",
-			type: "warning",
-		});
-		toast.add({
-			title: "Erro",
-			description: "Um exemplo de erro.",
-			type: "error",
-		});
-		toast.add({
-			title: "Erro",
-			description: "Um exemplo de erro.",
-			type: "error",
-		});
-		toast.add({
-			title: "Informação",
-			description: "Um exemplo de informação.",
-			type: "info",
-		});
-		toast.add({
-			title: "Carregando...",
-			description: "Um exemplo de carregamento.",
-			type: "loading",
-		});
-	}
+    const handleNuiToast = useCallback((data: ToastMessage) => {
+        toast.add({
+            title: data.title,
+            description: data.description,
+            type: data.type,
+        });
+    }, []);
 
-	return (
-		<main className="place-content-center gap-4 grid p-4 w-full min-h-svh">
-			<h1 className="shimmer shimmer-color-primary">RoseRP</h1>
-			<Button onClick={handleOpenToast}>Toast</Button>
-		</main>
-	);
+    useNuiEvent<ToastMessage>("toast", handleNuiToast);
+
+    function handleOpenToast() {
+        toast.add({
+            title: "Sucesso",
+            description: "Um exemplo de sucesso.",
+            type: "success",
+        });
+    }
+
+    return (
+        <main className="place-content-center gap-4 grid p-4 w-full min-h-svh">
+            <h1 className="shimmer shimmer-color-primary">RoseRP</h1>
+            <Button onClick={handleOpenToast}>Toast</Button>
+        </main>
+    );
 }
